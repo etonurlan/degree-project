f5c0e713e19480bcb04bdc4d104e5ce8ef5f529e
Nurlan <etonurlan@gmail.com>
2025-03-14 11:43:56 +0500

    Подключил веб-сокеты. Добавил ChatInput и возможность отправлять как текстовые сообщения, так и файлы(pdf, img)

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { db } from "@/lib/db";
 
 interface ChannelIdPageProps {
@@ -42,6 +43,15 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
