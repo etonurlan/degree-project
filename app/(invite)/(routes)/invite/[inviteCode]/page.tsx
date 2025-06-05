fb7a58913335f85c4b1b639c66fc24076556dd16
Nurlan <etonurlan@gmail.com>
2025-06-05 20:56:52 +0500

    Небольшое изменение для деплоя

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -8,11 +8,8 @@ interface InviteCodePageProps {
   params: {
     inviteCode: string;
   };
-};
-
-const InviteCodePage = async ({
-  params
-}: InviteCodePageProps) => {
+}
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
   const profile = await currentProfile();
 
   if (!profile) {
@@ -28,10 +25,10 @@ const InviteCodePage = async ({
       inviteCode: params.inviteCode,
       members: {
         some: {
-          profileId: profile.id
-        }
-      }
-    }
+          profileId: profile.id,
+        },
+      },
+    },
   });
 
   if (existingServer) {
@@ -47,17 +44,17 @@ const InviteCodePage = async ({
         create: [
           {
             profileId: profile.id,
-          }
-        ]
-      }
-    }
+          },
+        ],
+      },
+    },
   });
 
   if (server) {
     return redirect(`/servers/${server.id}`);
   }
-  
+
   return null;
-}
- 
+};
+
 export default InviteCodePage;
